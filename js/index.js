2163e156e3e0cddbb8ffa7089e0dd5e699bcf86d
lucasmasini <lucasmas31@gmail.com>
2022-02-25 03:04:17 -0300

    Comenzado con el buscador

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
+let searchItemsInput = document.querySelector(".searchInput").addEventListener("change", searchItemLive);
 
+
+const searchItemLive = (e) => {
+    let input = e.target;
+    
+    
+}
